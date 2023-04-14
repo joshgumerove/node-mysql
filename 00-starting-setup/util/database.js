66db64fbce443a57b56c256fb97e5d0f1b7e37f1
Josh Gumerove <jgumerove@kaufmanrossin.com>
2023-04-14 00:17:40 -0400

    sequelize setup

--- 00-starting-setup/util/database.js
+++ 00-starting-setup/util/database.js
@@ -1,10 +1,19 @@
-const mysql = require("mysql2");
+// const mysql = require("mysql2");
 
-const pool = mysql.createPool({
+// const pool = mysql.createPool({
+//   host: "localhost",
+//   user: "golden",
+//   password: "password",
+//   database: "node_complete",
+// });
+
+// module.exports = pool.promise();
+
+const Sequelize = require("sequelize");
+
+const sequelize = new Sequelize("node_complete", "golden", "password", {
+  dialect: "mysql",
   host: "localhost",
-  user: "golden",
-  password: "password",
-  database: "node_complete",
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
